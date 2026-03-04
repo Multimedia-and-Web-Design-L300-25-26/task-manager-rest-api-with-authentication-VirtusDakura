425a7a00443b84386afd6ed68beb195722444e0b
Virtus Dakura <dakuravirtus@gmail.com>
2026-03-04 12:04:31 +0000

    Implement task manager API with authentication

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
